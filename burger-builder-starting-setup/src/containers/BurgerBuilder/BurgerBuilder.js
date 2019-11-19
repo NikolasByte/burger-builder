a5ce42f830dd3e993210500e3333e0c0caf80386
Nikolas Charalambous <nikolas.c@hotmail.co.uk>
2019-11-19 22:25:31 +0000

    wip

--- burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,23 @@ import Burger from '../../components/Burger/Burger';
 
 
 class BurgerBuilder extends Component {
+
+
+    state = {
+        ingredidents: {
+            salad: 1,
+            bacon: 1,
+            cheese: 1,
+            meat: 2
+        }
+    }
+
+
+
     render () {
         return (
             <Aux>
-                <Burger/>
+                <Burger ingredidents={this.state.ingredidents}/>
                 <div>Build Controls</div>
             </Aux>
         );
